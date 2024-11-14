9427b60c89636f02345ea6147c4c70dae91352f1
tanvir55539 <md10vir@gmail.com>
2024-11-15 01:59:03 +0600

    added file comment

--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:meeting_app/utils/colors.dart';
 
+// A custom button widget that can be reused throughout the app.
+// It takes in a required `text` for the button label and a required
+// `onPressed` callback function to handle the button's press action.
+
 class CustomButton extends StatelessWidget {
   final String text;
   final VoidCallback onPressed;
@@ -16,7 +20,7 @@ class CustomButton extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(28.0),
       child: ElevatedButton(
-        onPressed: onPressed,
+        onPressed: onPressed, // Sets the function to call when the button is pressed.
         child: Text(
           text,
           style: const TextStyle(
