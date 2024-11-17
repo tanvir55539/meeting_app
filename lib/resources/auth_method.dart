2d2710f3936af534c6bc1a70df29e9b5362691b6
Your Name <sidratulpunno>
2024-11-17 18:48:05 +0600

    Added the authmethod

---
+++ lib/resources/auth_method.dart
@@ -0,0 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:meeting_app/utils/utils.dart';
+
+class AuthMethods {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Stream<User?> get authChanges => _auth.authStateChanges();
+
+  User? get user => _auth.currentUser;
+
+
+}
