b4bc85a3597f2fa1113f7501a70dd23d4c1444f9
sidratulpunno <sidratulmuntahapunno845000.com>
2024-12-08 18:59:19 +0600

    Added the file for jitsi meet which required to access the jitsi meet package

---
+++ lib/resources/jitsi_meet_methods.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:jitsi_meet_flutter_sdk/jitsi_meet_flutter_sdk.dart';
+import 'package:meeting_app/resources/auth_method.dart';
+import 'package:meeting_app/resources/firestore_methods.dart';
+
+class JitsiMeetMethods {
+  final AuthMethods _authMethods = AuthMethods();
+  final FireStoreMethods _fireStoreMethods = FireStoreMethods();
+
+
+  }
+}
