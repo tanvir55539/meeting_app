b1284b5a50a554a9a0cef244c3b836f31ef60dc5
tanvir55539 <md10vir@gmail.com>
2024-12-14 00:27:27 +0600

    Added toggle options to mute/unmute audio and video with SwitchListTile components.

--- lib/screens/video_call_screen.dart
+++ lib/screens/video_call_screen.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
-class VideoCallScreen extends StatelessWidget {
+class VideoCallScreen extends StatefulWidget {
   const VideoCallScreen({super.key});
+
+  @override
+  State<VideoCallScreen> createState() => _VideoCallScreenState();
+}
+
+class _VideoCallScreenState extends State<VideoCallScreen> {
+  bool isAudioMuted = true;
+  bool isVideoMuted = true;
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +29,25 @@ class VideoCallScreen extends StatelessWidget {
           InkWell(
             onTap: () {},
             child: const Text('Join'),
+          ),
+          const SizedBox(height: 20),
+          SwitchListTile(
+            title: const Text('Mute Audio'),
+            value: isAudioMuted,
+            onChanged: (val) {
+              setState(() {
+                isAudioMuted = val;
+              });
+            },
+          ),
+          SwitchListTile(
+            title: const Text('Mute Video'),
+            value: isVideoMuted,
+            onChanged: (val) {
+              setState(() {
+                isVideoMuted = val;
+              });
+            },
           ),
         ],
       ),
