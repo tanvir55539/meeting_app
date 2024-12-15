fc0390b5453cac304507f313323d726b00ba4d39
tanvir55539 <md10vir@gmail.com>
2024-12-15 19:25:24 +0600

    Finalized implementation: Integrated Jitsi Meet SDK, added authentication, and enhanced UI for meeting functionality.

--- lib/screens/video_call_screen.dart
+++ lib/screens/video_call_screen.dart
@@ -1,71 +1,140 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:jitsi_meet_flutter_sdk/jitsi_meet_flutter_sdk.dart';
+import 'package:meeting_app/resources/auth_method.dart';
+import 'package:meeting_app/resources/jitsi_meet_methods.dart';
+import 'package:meeting_app/utils/colors.dart';
+import 'package:meeting_app/widgets/meeting_option.dart';
 
 class VideoCallScreen extends StatefulWidget {
   const VideoCallScreen({super.key});
 
   @override
-  State<VideoCallScreen> createState() => _VideoCallScreenState();
+  State<VideoCallScreen> createState() {
+    return _VideoCallScreeState();
+  }
 }
 
-class _VideoCallScreenState extends State<VideoCallScreen> {
-  final TextEditingController meetingIdController = TextEditingController();
-  final TextEditingController nameController = TextEditingController();
-  bool isAudioMuted = true;
-  bool isVideoMuted = true;
+class _VideoCallScreeState extends State<VideoCallScreen> {
+  final AuthMethods _authMethods = AuthMethods();
+  late TextEditingController meetingIdController;
+  late TextEditingController nameController;
+  final JitsiMeetMethods _jitsiMeetMethods = JitsiMeetMethods();
 
-  void _joinMeeting() {
-    print('Joining meeting: Room ID: ${meetingIdController.text}, Name: ${nameController.text}, Audio Muted: $isAudioMuted, Video Muted: $isVideoMuted');
+  bool isAudioMuted = true;
+  bool isVedioMuted = true;
+
+  @override
+  void initState() {
+    meetingIdController = TextEditingController();
+    nameController = TextEditingController(
+      text: _authMethods.user.displayName,
+    );
+    super.initState();
   }
 
   @override
   void dispose() {
+    super.dispose();
     meetingIdController.dispose();
     nameController.dispose();
-    super.dispose();
+  }
+
+  _joinMeeting() {
+    _jitsiMeetMethods.createMeeting(
+      roomName: meetingIdController.text,
+      isAudioMuted: isAudioMuted,
+      isVideoMuted: isVedioMuted,
+      username: nameController.text,
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Join a meeting'),
+        elevation: 0,
+        backgroundColor: backgroundColor,
+        title: const Text(
+          'Join a meeting',
+          style: TextStyle(
+            fontSize: 20,
+          ),
+        ),
+        centerTitle: true,
       ),
       body: Column(
         children: [
-          TextField(
-            controller: meetingIdController,
-            decoration: const InputDecoration(hintText: 'Room ID'),
+          SizedBox(
+            height: 60,
+            child: TextField(
+              controller: meetingIdController,
+              maxLines: 1,
+              textAlign: TextAlign.center,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                  fillColor: secondaryBackgroundColor,
+                  filled: true,
+                  border: InputBorder.none,
+                  hintText: 'Room ID',
+                  contentPadding: EdgeInsets.fromLTRB(16, 8, 0, 0)),
+            ),
           ),
-          TextField(
-            controller: nameController,
-            decoration: const InputDecoration(hintText: 'Name'),
+          SizedBox(
+            child: TextField(
+              controller: nameController,
+              maxLines: 1,
+              textAlign: TextAlign.center,
+              keyboardType: TextInputType.text,
+              decoration: const InputDecoration(
+                  fillColor: secondaryBackgroundColor,
+                  filled: true,
+                  border: InputBorder.none,
+                  hintText: 'Name',
+                  contentPadding: EdgeInsets.fromLTRB(16, 8, 0, 0)),
+            ),
           ),
-          const SizedBox(height: 20),
+          const SizedBox(
+            height: 20,
+          ),
           InkWell(
             onTap: _joinMeeting,
-            child: const Text('Join'),
+            child: const Padding(
+              padding: EdgeInsets.all(8),
+              child: Text(
+                'Join',
+                style: TextStyle(fontSize: 16),
+              ),
+            ),
           ),
-          const SizedBox(height: 20),
-          SwitchListTile(
-            title: const Text('Mute Audio'),
-            value: isAudioMuted,
-            onChanged: (val) {
-              setState(() {
-                isAudioMuted = val;
-              });
-            },
+          const SizedBox(
+            height: 20,
           ),
-          SwitchListTile(
-            title: const Text('Mute Video'),
-            value: isVideoMuted,
-            onChanged: (val) {
-              setState(() {
-                isVideoMuted = val;
-              });
-            },
+          MeetingOption(
+            text: 'Mute audio',
+            isMute: isAudioMuted,
+            onChange: onAudioMuted,
+          ),
+          MeetingOption(
+            text: 'Turn of video',
+            isMute: isVedioMuted,
+            onChange: onVedioMuted,
           ),
         ],
       ),
     );
+  }
+
+  onAudioMuted(bool val) {
+    setState(() {
+      isAudioMuted = val;
+    });
+  }
+
+  onVedioMuted(bool val) {
+    setState(() {
+      isVedioMuted = val;
+    });
   }
 }
